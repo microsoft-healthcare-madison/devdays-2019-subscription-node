8e127e3d72c94691344e0ce923444dc18110fbd3
Gino Canessa <ginocanessa@gmail.com>
2020-05-12 13:26:19 -0500

    Updates for May 2020

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 A basic Node JS project for use in discovering the basics of FHIR R5 Subscriptions.
 
-Built for DevDays 2019 Europe.
+* Built for DevDays 2019 Europe.
+* Updated for DevDays 2020 US (May - Online)
 
 Meant to be used in conjunction with the test server at http://subscriptions.argo.run
 
--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const localListenPort = 32019;
 const publicUrl = '';
 
 /** define the FHIR server URL */
-const fhirServerUrl = 'https://server.subscriptions.argo.run';
+const fhirServerUrl = 'http://localhost:56340'; // 'https://server.subscriptions.argo.run';
 
 /** patient ID we are using (once created) */
 var patientId = '';
@@ -27,12 +27,10 @@ var notificationCount = 0;
 /** Function that runs this sever (required to be in function for async/await) */
 async function run() {
 
-  // **** start our local HTTP server ****
-
+  // start our local HTTP server
   startHttpListener();
 
-  // **** get a list of Topic resources ****
-
+  // get a list of Topic resources
   let topics = await getTopics();
 
   if (topics.length < 1) {
@@ -40,29 +38,25 @@ async function run() {
     process.exit(1);
   }
 
-  // **** list topics in the console ****
-
+  // list topics in the console
   console.log('Found SubscriptionTopics:');
   topics.forEach(topic => {
     console.log(` SubscriptionTopic/${topic.id} - ${topic.title}: ${topic.description} (${topic.url})`);
   });
 
-  // **** make sure our patient exists ****
-
+  // make sure our patient exists
   if (!await createPatientIfRequired()) {
     console.log(`Failed to verify patient: Patient/${patientId}`);
     process.exit(1);
   }
 
-  // **** create our subscription ****
-
+  // create our subscription
   if (!await createSubscription(topics[0])) {
     console.log('Failed to create subscription!');
     process.exit(1);
   }
 
-  // **** post an encounter, this program will exit when the notification is receieved ****
-
+  // post an encounter, this program will exit when the notification is receieved
   if (!await postEncounter()) {
     console.log('Failed to create encounter!');
     await deleteSubscription();
@@ -72,23 +66,17 @@ async function run() {
 
 /** Handle POST events on the /notification url */
 async function handleNotificationPost(req, res) {
-  // **** return generic OK ****
-
+  // return generic OK
   res.status(200).send();
 
-  // **** tell the user we received something ****
+  // tell the user we received something
+  console.log('Received POST on /notification');
 
-  console.log('Received POST on /notification')
-
-  // **** express has already parsed this for us ****
-
+  // express has already parsed this for us
   let bundle = req.body;
 
-  // **** dump the request body for the user ****
-
+  // dump the request body for the user
   // console.log('Body of POST:', bundle);
-
-  // **** attempt to parse the notification bundle ****
 
   try {
     let eventCount = NaN;
@@ -96,7 +84,38 @@ async function handleNotificationPost(req, res) {
 		let status = '';
 		let topicUrl = '' ;
     let subscriptionUrl = '';
-    
+    let notificationType = '';
+
+    // current R5 bundle
+    if ((bundle) &&
+        (bundle.type === 'subscription-notification') &&
+        (bundle.entry) &&
+        (bundle.entry.length > 0) &&
+        (bundle.entry[0].resource))
+      {
+        status = bundle.entry[0].resource.status;
+        topicUrl = bundle.entry[0].resource.topic.reference;
+        subscriptionUrl = bundle.entry[0].resource.subscription.reference;
+        notificationType = bundle.entry[0].resource.notificationType;
+        switch (notificationType) {
+          case 'handshake':
+            eventCount = 0;
+            bundleEventCount = 0;
+            break;
+  
+          case 'heartbeat':
+            eventCount = Number(bundle.entry[0].resource.eventsSinceSubscriptionStart);
+            bundleEventCount = 0;
+            break;
+  
+          case 'event-notification':
+            eventCount = Number(bundle.entry[0].resource.eventsSinceSubscriptionStart);
+            bundleEventCount = Number(bundle.entry[0].resource.eventsInNotification);
+            break;
+        }
+      }
+      
+    // last revision of R4 bundle (left for compatibility)
     if ((bundle) &&
         (bundle.meta) &&
         (bundle.meta.extension))
@@ -116,12 +135,10 @@ async function handleNotificationPost(req, res) {
       });
     }
 
-    // **** increment the number of notifications we have received ****
-
+    // increment the number of notifications we have received
     notificationCount++;
 
-    // **** check for being a handshake ****
-
+    // check for being a handshake
     if (eventCount === 0) {
       console.log(`Handshake:\n`+
         `\tSubscriptionTopic: ${topicUrl}\n` +
@@ -136,8 +153,7 @@ async function handleNotificationPost(req, res) {
       `\tTotal Events:      ${eventCount}`);
     }
     
-    // **** check if we are done ****
-
+    // check if we are done
     if (notificationCount === 2) {
       await deleteSubscription();
       process.exit(0);
@@ -150,14 +166,12 @@ async function handleNotificationPost(req, res) {
   }
 }
 
-// **** run our server ***
-
+// run our server
 run();
 
 /** POST an encounter to the server using our specified Patient ID */
 async function postEncounter() {
-  // **** create our encounter object ****
-
+  // create our encounter object
   let encounter = {
     resourceType: 'Encounter',
     class: {
@@ -169,9 +183,8 @@ async function postEncounter() {
       reference: `Patient/${patientId}`,
     }
   }
-  
-  // *** build the URL to POST this encounter ****
 
+  // build the URL to POST this encounter
   let url = new URL('Encounter?_format=json', fhirServerUrl).toString();
 
   try {
@@ -189,24 +202,19 @@ async function postEncounter() {
       return false;
     }
 
-    // **** grab the body ****
-    
+    // grab the body
     let body = await response.text();
 
-    // **** parse the body ****
-
+    // parse the body
     let enc = JSON.parse(body);
 
-    // **** grab the id so we can clean up ****
-
+    // grab the id so we can clean up
     encounterId = enc.id;
 
-    // **** log ****
-
+    // log
     console.log(`Created encounter: Encounter/${encounterId}`);
 
-    // **** success ****
-
+    // success
     return true;
   } catch (err) {
     console.log(`createSubscription: ${err}`);
@@ -239,42 +247,32 @@ async function deleteSubscription() {
 
 /** Create a subscription on the server */
 async function createSubscription(topic) {
-  // **** create our subscription object ****
-
+  // create our subscription object
   let subscription = {
-    resourceType: 'Subscription',
-    channel: {
+      resourceType: 'Subscription',
+      status: 'requested',
+      topic: {reference: topic.url},
+      end: '',
+      reason: 'DevDays Example - Node',
+      filterBy: [{
+        searchParamName: 'patient',
+        searchModifier: '=',
+        value: `Patient/${patientId}`,
+      }],
+      channelType: {
+        system: 'http://terminology.hl7.org/CodeSystem/subscription-channel-type',
+        code: 'rest-hook'
+      },
       endpoint: publicUrl ? publicUrl : `http://localhost:${localListenPort}/notification`,
       header: [],
       heartbeatPeriod: 60,
-      payload: {
-        content: 'id-only',
-        contentType: 'application/fhir+json',
-      },
-      type: {
-        coding: [ {
-          code: 'rest-hook',
-          display: 'Rest Hook',
-          system: 'http://terminology.hl7.org/CodeSystem/subscription-channel-type'
-        }],
-        text: 'REST Hook',
-      },
-    },
-    filterBy: [{
-      matchType: '=',
-      searchParamName: 'patient',
-      value: `Patient/${patientId}`,
-    }],
-    end: '',
-    topic: {reference: topic.url},
-    reason: 'DevDays Example - Node',
-    status: 'requested'
-  }
+      content: 'id-only',
+      contentType: 'application/fhir+json'
+    };
   
-  // console.log('Subscription:', subscription);
+  //console.log('Subscription:', subscription);
 
-  // *** build the URL to POST this subscription ****
-
+  // build the URL to POST this subscription
   let url = new URL('Subscription', fhirServerUrl).toString();
 
   try {
@@ -292,24 +290,20 @@ async function createSubscription(topic) {
       return false;
     }
 
-    // **** grab the body ****
-    
+    // grab the body
     let body = await response.text();
+    //console.log('Response:', body);
 
-    // **** parse the body ****
-
+    // parse the body
     let sub = JSON.parse(body);
 
-    // **** grab the id so we can clean up ****
-
+    // grab the id so we can clean up
     subscriptionId = sub.id;
 
-    // **** log ****
-
+    // log
     console.log(`Created subscription: Subscription/${subscriptionId}`);
 
-    // **** success ****
-
+    // success
     return true;
   } catch (err) {
     console.log(`createSubscription: ${err}`);
@@ -319,46 +313,39 @@ async function createSubscription(topic) {
 
 /** Get a Bundle of Topics from a FHIR server */
 async function getTopics() {
-  // **** build the URL to the server ****
-
+  // build the URL to the server
   let url = new URL('SubscriptionTopic', fhirServerUrl).toString();
 
   try {
-    // **** fetch the Topics ****
-
+    // fetch the SubscriptionTopics
     let response = await fetch(url, {
       method: 'GET',
       headers: {'Accept': 'application/fhir+json'},
     });
     let body = await response.text();
 
-    // **** check for success ****
-
+    // check for success
     if (!response.ok) {
       return [];
     }
 
-    // **** parse the JSON ****
-
+    // parse the JSON
     let bundle = JSON.parse(body);
     let topics = [];
 
-    // **** check for values ****
-
+    // check for values
     if (!bundle.entry) {
       return [];
     }
 
-    // **** traverse topics ****
-
+    // traverse topics
     bundle.entry.forEach(entry => {
       if (!entry.resource) return;
 
       topics.push(entry.resource);
     });
 
-    // **** return our list ****
-
+    // return our list
     return topics;
   } catch (err) {
     console.log(`getTopics: ${err}`);
@@ -368,12 +355,10 @@ async function getTopics() {
 
 /** Creates a patient record in case we need one */
 async function createPatientIfRequired() {
-  // **** create a patient id ****
-
+  // create a patient id
   patientId = uuid();
   
-  // **** build the URL to check for our patient ****
-
+  // build the URL to check for our patient
   let url = new URL(`Patient/${patientId}`, fhirServerUrl);
 
   let response = await fetch(url, {
@@ -382,18 +367,15 @@ async function createPatientIfRequired() {
   });
   let body = await response.text();
 
-  // **** check for failure ****
-
+  // check for failure
   if (!response.ok) {
     return (await createPatient());
   }
 
-  // **** parse the JSON ****
-
+  // parse the JSON
   let bundle = JSON.parse(body);
   
-  // **** check for values ***
-
+  // check for values
   if (!bundle.entry) {
     return (await createPatient());
   }
@@ -402,15 +384,13 @@ async function createPatientIfRequired() {
     return (await createPatient());
   }
 
-  // **** done ****
-
+  // done
   return true;
 }
 
 /** Create a basic patient on the FHIR server via PUT */
 async function createPatient() {
-  // **** build a basic patient record ****
-
+  // build a basic patient record
   let patient = {
     resourceType: 'Patient',
     id: patientId,
@@ -423,8 +403,7 @@ async function createPatient() {
     birthDate: '2019-11-20'
   };
 
-  // *** build the URL for this patient ****
-
+  // build the URL for this patient
   let url = new URL(`Patient/${patientId}?_format=json`, fhirServerUrl).toString();
 
   try {
@@ -442,8 +421,7 @@ async function createPatient() {
       console.log(`createPatient: url: ${url} returned: ${response.status}`)
     }
     
-    // **** check for success ****
-
+    // check for success
     return response.ok;
   } catch (err) {
     console.log(`createPatient: ${err}`);
@@ -455,43 +433,36 @@ async function createPatient() {
 /** Configure and Start the HTTP listener */
 function startHttpListener() {
 
-  // **** build our app ****
-
+  // build our app
   const app = express();
 
-  // **** use CORS (default is allow everything when enabled) ****
-
+  // use CORS (default is allow everything when enabled)
   app.use(cors());
 
-  // **** have express decode URLs ****
-
+  // have express decode URLs
   app.use(
     express.urlencoded({
       extended: true
     })
   );
 
-  // **** have express do basic JSON parsing ****
-
+  // have express do basic JSON parsing
   app.use(express.json({type: ['application/json', 'application/fhir+json']}));
 
-  // **** configure root handler people can make sure it works ****
-
+  // configure root handler people can make sure it works
   app.get('/', async (req, res) => {
     res.send('Server is alive and listening...');
   });
 
   app.post('/notification', handleNotificationPost)
 
-  // **** configure 404 handler ****
-
+  // configure 404 handler
   // eslint-disable-next-line no-unused-vars
   app.use(function(req, res, next) {
     res.status(404).send('404 - Not Found');
   });
 
-  // **** listen on the default port or the one sepcified ****
-
+  // listen on the default port or the one sepcified
   app.listen(
     localListenPort, 
     () => console.log(`Listening on http://localhost:${localListenPort}`)
